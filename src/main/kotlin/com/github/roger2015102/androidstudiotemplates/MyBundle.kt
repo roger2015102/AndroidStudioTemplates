a06f8f0385464dd58869592c3a51b1048b602bcf
GitHub Action <action@github.com>
2024-04-08 10:17:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/roger2015102/androidstudiotemplates/MyBundle.kt
+++ src/main/kotlin/com/github/roger2015102/androidstudiotemplates/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.roger2015102.androidstudiotemplates
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/roger2015102/androidstudiotemplates/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/roger2015102/androidstudiotemplates/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.roger2015102.androidstudiotemplates.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
